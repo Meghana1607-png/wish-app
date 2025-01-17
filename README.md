422a84763588c997e754070c92072fd0f8787d13
Meghana1607-png <meghanavajrapu2007@gmail.com>
2025-01-17 07:10:15 +0530

    waste cleared

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Wishapp
+# blood_bridge
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.14.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wishapp'`, () => {
+  it(`should have as title 'blood_bridge'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wishapp');
+    expect(app.title).toEqual('blood_bridge');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wishapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('blood_bridge app is running!');
   });
 });
