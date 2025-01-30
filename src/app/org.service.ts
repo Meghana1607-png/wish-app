ecb3e72b3698fbbcf009d80f3b11fa7c227d87c3
chandini chand <chandini.chand@default.com>
2025-01-30 08:24:22 +0530

    merge commit

--- src/app/org.service.ts
+++ src/app/org.service.ts
@@ -10,52 +10,42 @@ export class OrgService {
   private supabase: SupabaseClient;
 
   private apiurl= 'http://localhost:3000/orgforminsert';
-  // private api = 'http://localhost:3000/orgformfetch';
   private api = 'http://localhost:3000/orgSignIn';
+  private bloodGroupApi = 'http://localhost:3000/bloodgroups'; // Endpoint for blood groups
   
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) { 
     auth: {
       persistSession: false
       //  Disables session persistence and lock
     }
-
     this.supabase = createClient("https://esuzqpwibfnycwmeirtg.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdXpxcHdpYmZueWN3bWVpcnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NjA1MTQsImV4cCI6MjA1MDUzNjUxNH0.FUL9viBXkN2Q44hhdFKPj8uKBT0SkJqcSfbjPV2oExc")
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdXpxcHdpYmZueWN3bWVpcnRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NjA1MTQsImV4cCI6MjA1MDUzNjUxNH0.FUL9viBXkN2Q44hhdFKPj8uKBT0SkJqcSfbjPV2oExc")
   }
 
   Orginsert(org: any): Observable<any> {
-    console.log('Sending data to API:', org);
     return this.http.post(this.apiurl, org);
   }
 
   OrgSignIn(data: any): Observable<any> {
-    console.log('Sending data to API:', data);
     return this.http.post(this.api, data);
   }
-  // async fetchorgform(): Promise<any>{
-  //   const {data,error}= await this.supabase.from('organization')
-  //   .select('name,email')
-  //   // .eq('org_id', '9e41a25c-e9e2-4d85-af34-16aa103a50b7');
-  //   if(data){
-  //     console.log("fetched org",data);
-  //     return data;
-  //   }
-  //   console.log("error fetching org",error)
-  //   return null;
-  // }
-  fetchorgform(id:any): Observable<any> {
+
+  fetchorgform(id: any): Observable<any> {
     return from(
       this.supabase
         .from('organization')
-        .select().eq('id',id)
+        .select().eq('id', id)
         .then((response) => {
-          console.log('Supabase Response:', response);
           return response;
         })
     );
   }
-  
+
+  getBloodGroups(): Observable<any> {
+    return this.http.get(this.bloodGroupApi); // Fetch blood group data
+  }
+
+  addBloodGroup(bloodGroupData: { name: string; units: number }): Observable<any> {
+    return this.http.post(this.bloodGroupApi, bloodGroupData); // Add blood group data
+  }
 }
-
-
-  
