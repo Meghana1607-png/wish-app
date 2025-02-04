d01448dca8998ee1267d5489df72af4e9f537fe1
sriharshitha16 <sriharshithapattisapu@gmail.com>
2025-02-04 14:16:21 +0530

    donor form

--- src/app/donor/donor.component.ts
+++ src/app/donor/donor.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Validators,FormBuilder,FormGroup} from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-donor',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./donor.component.css']
 })
 export class DonorComponent {
+  donorForm: FormGroup;
+  currentStep = 1;
+  bloodGroups = ['A+', 'A-', 'B+', 'B-', 'O+', 'O-', 'AB+', 'AB-'];
 
+  constructor(private fb: FormBuilder) {
+    this.donorForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      mobile: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(65)]],
+      address: ['', Validators.required],
+      bloodGroup: ['', Validators.required],
+      healthIssues: [''],
+      lastDonatedDate: ['']
+    });
+}
+goToNextStep() {
+  if (this.isStepValid(1)) {
+    this.currentStep = 2;
+  }
+}
+
+isStepValid(step: number): boolean {
+  if (step === 1) {
+    return this.donorForm.controls['username'].valid &&
+           this.donorForm.controls['email'].valid &&
+           this.donorForm.controls['password'].valid;
+  }
+  return true;
+}
+
+submitForm() {
+  if (this.donorForm.valid) {
+    console.log('Donor Form Submitted', this.donorForm.value);
+  }
+}
 }
